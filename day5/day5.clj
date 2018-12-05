001546ae2e445f7e84215329d4b3e993dec229c0
Johannes Norrbacka <johannes.norrbacka@hotmail.com>
2018-12-05 21:25:20 +0100

    Fixat uppgift 2, easy game

--- day5/day5.clj
+++ day5/day5.clj
@@ -9,13 +9,26 @@
 
 (def pattern #"Aa|aA|Bb|bB|Cc|cC|Dd|dD|Ee|eE|Ff|fF|Gg|gG|Hh|hH|Ii|iI|Jj|jJ|Kk|kK|Ll|lL|Mm|mM|Nn|nN|Oo|oO|Pp|pP|Qq|qQ|Rr|rR|Ss|sS|Tt|tT|Uu|uU|Vv|vV|Ww|wW|Xx|xX|Yy|yY|Zz|zZ")
 
-(defn react [str]
+(defn react [str p]
   (loop [s str]
-    (let [next (clojure.string/replace s pattern "")]
+    (let [next (clojure.string/replace s p "")]
       (if (= s next)
         s
         (recur next)))))
 
 (def input (clojure.string/join "" (slurp "newinput.txt")))
 
-(count (react input))
+(count (react input pattern))
+
+(def patternArray [#"A|a" #"B|b" #"C|c" #"D|d" #"E|e" #"F|f" #"G|g" #"H|h" #"I|i" #"J|j" #"K|k" #"L|l" #"M|m" #"N|n" #"O|o" #"P|p" #"Q|q" #"R|r" #"S|s" #"T|t" #"U|u" #"V|v" #"W|w" #"X|x" #"Y|y" #"Z|z"])
+
+(defn reactSmallest [str]
+  (for [p patternArray]
+    (apply assoc {} [
+        :pattern p
+        :count (count (react (react str p) pattern))
+    ])    
+  )
+)
+
+(first (sort-by (fn [v] (-> v :count)) (reactSmallest input)))
